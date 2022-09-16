f94b4725d86c11f4902c56aae5655b244431657c
Hideki Miyamoto <hideki0145@gmail.com>
2022-09-17 07:58:11 +0900

    Update darwin's setup.sh file
    Add git.

--- src/darwin/setup.sh
+++ src/darwin/setup.sh
@@ -16,6 +16,32 @@ else
 fi
 echo "********************"
 
+# git
+echo "***** git *****"
+if ! has "git"; then
+  brew install git
+else
+  git --version
+fi
+if [ -z "`git config --list | grep user.name`" ]; then
+  git config --global user.name "Hideki Miyamoto"
+fi
+if [ -z "`git config --list | grep user.useConfigOnly`" ]; then
+  git config --global user.useConfigOnly true
+fi
+if [ -z "`git config --list | grep color.ui`" ]; then
+  git config --global color.ui auto
+fi
+if [ -z "`git config --list | grep pull.rebase`" ]; then
+  git config --global pull.rebase false
+fi
+git --no-pager config --global --list
+if [ ! -d "$HOME/.config/git" ]; then
+  mkdir -p "$HOME/.config/git"
+fi
+ln -snfv "$DOT_DIR/config/git/ignore" "$HOME/.config/git/ignore"
+echo "***************"
+
 
 # Setup complete
 readonly FIRST_RUN="$DOT_DIR/tmp/first_run"
