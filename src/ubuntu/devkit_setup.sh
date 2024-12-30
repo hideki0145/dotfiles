9b2f2c50ca257bdd40c6ea0400f5b89d81d0bc09
Hideki Miyamoto <hideki0145@gmail.com>
2024-12-31 00:54:40 +0900

    Update ubuntu's devkit_setup.sh file
    Update ubuntu's how to install gh.

--- src/ubuntu/devkit_setup.sh
+++ src/ubuntu/devkit_setup.sh
@@ -57,11 +57,13 @@ else
 fi
 
 # gh
+# For reference, see: https://github.com/cli/cli/blob/trunk/docs/install_linux.md
 package_name "gh"
 if ! has "gh"; then
-  type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)
+  type -p wget >/dev/null || (sudo apt update && sudo apt install -y wget)
   sudo mkdir -p -m 755 /etc/apt/keyrings
-  wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null
+  out=$(mktemp) && wget -nv -O"$out" https://cli.github.com/packages/githubcli-archive-keyring.gpg
+  sudo cat "$out" | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null
   sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
   sudo apt update
