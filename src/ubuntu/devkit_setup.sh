6b91da0b349aa1daad0e45d60c8dc580fd5baeda
Hideki Miyamoto <hideki0145@gmail.com>
2026-08-21 19:38:51 +0900

    Update ubuntu's devkit_setup.sh file
    Update ubuntu's how to install gh.

--- src/ubuntu/devkit_setup.sh
+++ src/ubuntu/devkit_setup.sh
@@ -56,14 +56,15 @@ fi
 redis-cli --version
 
 # gh
-# For reference, see: https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+# For reference, see: https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian
 package_name "gh"
 if ! has "gh"; then
   type -p wget >/dev/null || (sudo apt update && sudo apt install -y wget)
   sudo mkdir -p -m 755 /etc/apt/keyrings
   out=$(mktemp) && wget -nv -O"$out" https://cli.github.com/packages/githubcli-archive-keyring.gpg
-  sudo cat "$out" | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null
+  cat "$out" | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null
   sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+  sudo mkdir -p -m 755 /etc/apt/sources.list.d
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
   sudo apt update
   sudo apt install -y gh
