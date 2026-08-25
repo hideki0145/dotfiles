63fcd92784a944373db53bb0827e67254dc96740
Hideki Miyamoto <hideki0145@gmail.com>
2026-08-25 23:09:00 +0900

    Update ubuntu's and darwin's package_setup.sh files
    Update ubuntu's and darwin's how to install uv.

--- src/ubuntu/package_setup.sh
+++ src/ubuntu/package_setup.sh
@@ -26,10 +26,12 @@ fi
 package_name "uv"
 if ! has "uv"; then
   curl -LsSf https://astral.sh/uv/install.sh | sh
+  # shellcheck source=/dev/null
+  source "$HOME/.local/bin/env"
 else
+  uv self update
   uv --version
 fi
-uv self update
 uv generate-shell-completion zsh | tee ~/.zsh/completions/_uv >/dev/null
 uvx --generate-shell-completion zsh | tee ~/.zsh/completions/_uvx >/dev/null
 
